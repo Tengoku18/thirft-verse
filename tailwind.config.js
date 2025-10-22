195bd526a2e5c2ac52ff2043d16116dbbe167736
nikhilpantha <nikhilpantha207@gmail.com>
2025-10-22 09:20:15 +0545

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"], // Adjust paths as needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  presets: [require("nativewind/preset")],
+};
